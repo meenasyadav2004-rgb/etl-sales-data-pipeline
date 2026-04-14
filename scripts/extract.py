b57502ac81ed45393560d70ef2e04368fd9d793c
meenasyadav2004-rgb <meena.s.yadav2004@gmail.com>
2026-04-14 12:45:50 +0530

    Add files via upload

---
+++ scripts/extract.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+def extract():
+    try:
+        BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+        file_path = os.path.join(BASE_DIR, "data", "sales_data.csv")
+
+        print("Reading file from:", file_path)
+
+        df = pd.read_csv(file_path)
+        print("Extract successful")
+        return df
+
+    except Exception as e:
+        print("Error in extract:", e)
+        return None
+
+
+# 👇 ADD THIS PART (to run file directly)
+if __name__ == "__main__":
+    df = extract()
+    
+    if df is not None:
+        print(df.head())
+    else:
+        print("No data to display")
